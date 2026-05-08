96de3596a113a0edaa7c37f2e5822140b889a4f1
Nathan Boyer <nathanrobertboyer@gmail.com>
2026-05-08 11:41:00 -0400

    Moved existing equation data and notebooks from separate packages to this package.

--- src/Div3.jl
+++ src/Div3.jl
@@ -1,5 +1,25 @@
+"""
+    Package Div3 v$(pkgversion(Div3))
+
+A collection of equations from The American Society of Mechanical Engineers (ASME)
+Boiler and Pressure Vessel Code (BPVC) Section VIII Division 3 (Div3).
+This package is intended to be used as a dependency of other data processing packages.
+Equations are organized into modules named by their source location in Division 3.
+The equations may be viewed in rendered form by opening their `src/*_notebook.jl` file with Pluto.jl
+or by opening their exported `*_notebook.html` file in your web browser.
+"""
 module Div3
 
-# Write your package code here.
+module KM620
+
+include("KM620_notebook.jl")
+
+end #module
+
+module KD232
+
+include("KD232_notebook.jl")
+
+end #module
 
 end
